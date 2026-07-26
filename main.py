9b3bf214b303b61c25c91ae776b00c31ed2842cf
Qtgcy08 <cr200j5022@outlook.com>
2026-07-26 21:15:23 +0800

    chore: add AGPL-3.0 license

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 """
 astrbot_plugin_grep_history_context
 ====================================
+Copyright (C) 2026 依轨泠QTY (Qtgcy08)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 提供 grepHistoryContext 工具，供 LLM 搜索本 AstrBot 实例内所有对话上下文。
 
 V3 改进（2026-07-26）：
